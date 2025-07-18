2ce17e61e55398d415b043978028126d8edbbbbb
Husanbek <buxorojahon@gmail.com>
2025-07-18 18:40:22 +0500

    onrender

--- admin/src/middlewares/Fetch.tsx
+++ admin/src/middlewares/Fetch.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const Fetch = axios.create({
-  // baseURL: "https://delivery-server-3zkl.onrender.com/api/",
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://tdp21com.onrender.com/",
+  // baseURL: "http://localhost:4000/api/",
   headers: {
     Authorization: token,
   },
--- frontend/src/middlewares/Axios.tsx
+++ frontend/src/middlewares/Axios.tsx
@@ -2,5 +2,6 @@ import axios from "axios";
 
 
 export const Fetch = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  // baseURL: "http://localhost:4000/api/",
+  baseURL: "https://tdp21com.onrender.com/",
 });
